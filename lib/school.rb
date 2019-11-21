2445b20f9d365f616b8334a0bcfd8b68f7ef58e8
Daniel Reina <dannyflatiron@gmail.com>
2019-11-21 02:46:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+class School
+  attr_reader :name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade]<< name
+    # binding.pry
+  end
+
+  def grade(grade)
+    @roster[grade]
+    # binding.pry
+  end
+
+  def sort
+    # @roster.sort.to_h
+    @roster.each do |key, value|
+      @roster[key]=value.sort
+    end
+    # binding.pry
+  end
+end
