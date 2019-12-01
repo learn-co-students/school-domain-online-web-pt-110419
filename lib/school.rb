c547a750fc36cba7e662f6ddd90977c0cf5936ca
Dimitri Gedda <gratitudeadore@fastmail.com>
2019-12-01 23:22:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  
+  attr_reader :school
+  attr_accessor :roster
+  
+def initialize(school, roster = {})
+  @school = school
+  @roster = roster
+end
+
+def add_student(name, grade)
+	if @roster.has_key?(grade)
+	@roster[grade] << name
+	else
+		@roster[grade] = []
+		@roster[grade] << name
+	end
+end
+
+def grade(grade)
+  return @roster[grade]
+end
+def sort
+  return @roster.each_value{|value| value.sort![0][1]}
+  
+end
+
+end
