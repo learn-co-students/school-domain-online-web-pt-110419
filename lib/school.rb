f6c170d0d935699af9664844318fb3d1e366cd81
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-11 19:08:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  school = School.new("Bayside High School")
+  school.roster
+
+  school.add_student("Zach Morriss", 9)
+  school.roster
+  
+  school.add_student("AC Slater", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
+  school.roster
+  
+  school.grade(9)
+  
+  school.sort 
+  
